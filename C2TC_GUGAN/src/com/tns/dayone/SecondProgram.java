8fa477ef2b26770f6008f23cbe93b3fd2157895d
gugan <gugan@GUGAN>
2024-09-04 12:30:19 +0530

    This is Second task

---
+++ C2TC_GUGAN/src/com/tns/dayone/SecondProgram.java
@@ -0,0 +1,54 @@
+package com.tns.dayone;
+
+public class SecondProgram {
+	
+	public static void main (String[]args) {
+		
+	//if  program
+	i=10;
+	if(i==10);
+		System.out.println("ITS CORRECt")
+		
+		
+	// if else statement
+	if(y==10);
+		System.out.println("ITS TRUE")
+	else;
+		System.out.println("ITS FALSE")
+		
+		
+	//if else if statement
+		g=78;
+		if(g==65);
+			System.out.println("ITS 65")
+		else if(g==75);
+			System.out.println("ITS 75")
+		else if(g==78);
+			System.out.println("ITS 78")
+		else;
+			System.out.println("NOT AVAILABLE")
+			
+	// switch statement
+			switch (day=1):
+				case 1:
+					System.out.println("Monday")
+					break;
+				case 2:
+					System.out.println("Tuesday")
+					break;
+				case 3:
+					System.out.println("Wednesday")
+					break;
+				case 4:
+					System.out.println("Thursday")
+				case 5:
+					System.out.println("Friday")
+
+			}
+
+		
+
+		}
+			
+
+		}
---
+++ C2TC_GUGAN/src/com/tns/daytwo/conditionalstatementsdaytwo.java
@@ -0,0 +1,52 @@
+package com.tns.daytwo;
+
+public class conditionalstatementsdaytwo {
+
+	public static void main(String[] args) {
+		//if  program
+			 int h=10;
+				if(h==10)
+					System.out.println("ITS CORRECt");
+			
+		// if else statement
+		int y=80;
+		if(y==10)
+			System.out.println("ITS TRUE");
+		else
+			System.out.println("ITS FALSE");
+			
+			
+		//if else if statement
+			int g=78;
+			if(g==65)
+				System.out.println("ITS 65");
+			else if(g==75)
+				System.out.println("ITS 75");
+			else if(g==78)
+				System.out.println("ITS 78");
+			else
+				System.out.println("NOT AVAILABLE");
+				
+		// switch statement
+			int day=4;
+				switch (day=1) {
+					case 1:
+						System.out.println("Monday");
+						break;
+					case 2:
+						System.out.println("Tuesday");
+						break;
+					case 3:
+						System.out.println("Wednesday");
+						break;
+					case 4:
+						System.out.println("Thursday");
+						break;
+					case 5:
+						System.out.println("Friday");
+						break;
+				}
+
+	}
+
+}
