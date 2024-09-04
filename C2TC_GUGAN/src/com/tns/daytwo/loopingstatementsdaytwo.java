27b7bd2f6d5234a87f7506436f8544b35516c9b2
gugan <gugan@GUGAN>
2024-09-04 14:25:26 +0530

    THIS IS ABOUT THE LOOPING STATEMENTS

---
+++ C2TC_GUGAN/src/com/tns/daytwo/loopingstatementsdaytwo.java
@@ -0,0 +1,27 @@
+package com.tns.daytwo;
+
+public class loopingstatementsdaytwo {
+
+	public static void main(String[] args) {
+		///for loop
+		for (int i=0; i<=10; i++) {
+			System.out.println(i);
+		}
+	
+	//while loop
+		int h=0;
+		while (h < 10) {
+			System.out.println(h);
+			h++;
+		}
+		
+	//Do While Loop
+		int t=0;
+		do {
+			System.out.println(t);
+			t++;
+		}
+		while(t<3);
+		
+	}	
+}
